ce75d6ab6a39b730d5ad7d47c410b6a92c61fef0
Chaitali <chaitalipatel18j@gmail.com>
2016-02-13 13:01:58 +1300

    Add Comments

--- Gemfile
+++ Gemfile
@@ -49,4 +49,6 @@ end
 gem 'devise', '~> 3.5', '>= 3.5.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'acts_as_votable', '~> 0.10.0'
+gem 'simple_form', '~> 3.2', '>= 3.2.1'
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   root to: "links#index"
 
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
       put "like", to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
